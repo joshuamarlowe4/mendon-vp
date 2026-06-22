f06b7b540133f5673b3965dd2795bc57365dd9da
joshuamarlowe4 <joshuamarlowe4@gmail.com>
2026-06-22 15:21:35 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCidf3OkLiytITr-fhmIE34gNGaNj3cHmE",
+  authDomain:        "mendonvp.firebaseapp.com",
+  projectId:         "mendonvp",
+  storageBucket:     "mendonvp.firebasestorage.app",
+  messagingSenderId: "755324672734",
+  appId:             "1:755324672734:web:53a81c05ad1db31ee2404f"
+};
